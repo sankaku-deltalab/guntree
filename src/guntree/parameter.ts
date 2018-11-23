bd01b6851fa9817bac88cf924dd6a76112d82ec1
Sankaku <sankaku_dlt.45631@outlook.jp>
2018-11-24 00:36:31 +0900

    Update Parameter to can add value with multiplier

--- src/guntree/parameter.ts
+++ src/guntree/parameter.ts
@@ -1,11 +1,24 @@
 export class Parameter {
     private value: number;
+    private addingMultiplier: number;
 
     constructor(initialValue: number) {
         this.value = initialValue;
+        this.addingMultiplier = 1;
     }
 
     calcValue(): number {
         return this.value;
+    }
+
+    add(adding: number): number {
+        const actual = adding * this.addingMultiplier;
+        this.value += actual;
+        return actual;
+    }
+
+    multiplyLaterAdding(multiplier: number): number {
+        this.addingMultiplier *= multiplier;
+        return this.addingMultiplier;
     }
 }
--- src/test/parameter.test.ts
+++ src/test/parameter.test.ts
@@ -11,4 +11,39 @@ describe('#Parameter', () => {
         // Then parameter has initial value
         expect(parameter.calcValue()).toBe(initialValue);
     });
+
+    test('can add value', () => {
+        // Given parameter
+        const initialValue = 123;
+        const parameter = new Parameter(initialValue);
+
+        // When add parameter
+        const adding = 32;
+        parameter.add(adding);
+
+        // Then parameter was added
+        expect(parameter.calcValue()).toBe(initialValue + adding);
+    });
+
+    test('can add value with multiplier', () => {
+        // Given parameter
+        const initialValue = 123;
+        const parameter = new Parameter(initialValue);
+
+        // When update adding multiplier
+        const mlt = 1.2;
+        parameter.multiplyLaterAdding(mlt);
+
+        // And add parameter twice
+        const adding = [32, 54];
+        for (const a of adding) {
+            const added = parameter.add(a);
+
+            // Then added value was multiplier
+            expect(added).toBeCloseTo(a * mlt);
+        }
+
+        // Then parameter was added with multiplier
+        expect(parameter.calcValue()).toBeCloseTo(initialValue + (adding[0] + adding[1]) * mlt);
+    });
 });
