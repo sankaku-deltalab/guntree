ff0356b5387d8937e05388cf5985036c298559f7
Sankaku <sankaku_dlt.45631@outlook.jp>
2018-11-24 15:29:20 +0900

    Update Add gun to can use lazy-evaluative to adding value

--- src/guntree/gun-modifier.ts
+++ src/guntree/gun-modifier.ts
@@ -1,12 +1,18 @@
 import { IFiringState, IGun } from 'guntree/gun';
+import { ILazyEvaluative } from 'guntree/lazy-evaluative';
 
 export class Add implements IGun {
     constructor(private readonly name: string,
-                private readonly adding: number) {}
+                private readonly adding: number | ILazyEvaluative<number>) {}
 
     *play(state: IFiringState): IterableIterator<void> {
         const param = state.parameters.get(this.name);
         if (param === undefined) throw new Error();  // TODO:
-        param.add(this.adding);
+        param.add(this.calcAdding(state));
+    }
+
+    private calcAdding(state: IFiringState): number {
+        if (typeof this.adding === 'number') return this.adding;
+        return this.adding.calc(state);
     }
 }
--- src/test/test-gun-modifier/add.test.ts
+++ src/test/test-gun-modifier/add.test.ts
@@ -1,6 +1,7 @@
 import { Parameter } from 'guntree/parameter';
 import { IFiringState } from 'guntree/gun';
 import { Add } from 'guntree/gun-modifier';
+import { ILazyEvaluative } from 'guntree/lazy-evaluative';
 
 describe('#Add', () => {
     test('can add parameter with constant number', () => {
@@ -18,6 +19,40 @@ describe('#Add', () => {
         // And Add
         const adding = 2;
         const add = new Add(parameterName, adding);
+
+        // When play Add with one frame
+        const progress = add.play(state);
+        const result = progress.next();
+
+        // Then parameter has added only once
+        expect(parameter.add).toBeCalledTimes(1);
+        expect(parameter.add).toBeCalledWith(adding);
+
+        // And progress was finished
+        expect(result.done).toBe(true);
+    });
+
+    test('can add parameter with lazy-evaluative', () => {
+        // Given FiringState
+        const parameterClass = jest.fn<Parameter>(() => ({
+            add: jest.fn(),
+        }));
+        const parameterName = 'a';
+        const parameter = new parameterClass();
+        const firingStateClass = jest.fn<IFiringState>(() => ({
+            parameters: new Map<string, Parameter>([[parameterName, parameter]]),
+        }));
+        const state = new firingStateClass();
+
+        // And lazy-evaluative
+        const adding = 2;
+        const leClass = jest.fn<ILazyEvaluative<number>>(() => ({
+            calc: jest.fn().mockReturnValueOnce(adding),
+        }));
+        const le = new leClass();
+
+        // And Add
+        const add = new Add(parameterName, le);
 
         // When play Add with one frame
         const progress = add.play(state);
