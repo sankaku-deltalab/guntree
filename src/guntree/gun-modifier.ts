8eca8a00404e21d583a5ade23c09a32b025e3787
Sankaku <sankaku_dlt.45631@outlook.jp>
2018-11-24 15:52:43 +0900

    Add Multiply gun

--- src/guntree/gun-modifier.ts
+++ src/guntree/gun-modifier.ts
@@ -23,3 +23,26 @@ export class Add implements IGun {
         return this.adding.calc(state);
     }
 }
+
+/**
+ * Multiply parameter.
+ */
+export class Multiply implements IGun {
+    /**
+     * @param name paramter name
+     * @param multiplier multiplier value or lazy-evaluative deal multiplier value
+     */
+    constructor(private readonly name: string,
+                private readonly multiplier: number | ILazyEvaluative<number>) {}
+
+    *play(state: IFiringState): IterableIterator<void> {
+        const param = state.parameters.get(this.name);
+        if (param === undefined) throw new Error(`Parameter ${this.name} is not exist`);
+        param.multiply(this.calcMultiplier(state));
+    }
+
+    private calcMultiplier(state: IFiringState): number {
+        if (typeof this.multiplier === 'number') return this.multiplier;
+        return this.multiplier.calc(state);
+    }
+}
