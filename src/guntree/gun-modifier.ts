895d76a35f63aa36e95c02f0f61684ac02500a22
Sankaku <sankaku_dlt.45631@outlook.jp>
2018-11-24 15:44:44 +0900

    Update Add gun comment

--- src/guntree/gun-modifier.ts
+++ src/guntree/gun-modifier.ts
@@ -1,7 +1,14 @@
 import { IFiringState, IGun } from 'guntree/gun';
 import { ILazyEvaluative } from 'guntree/lazy-evaluative';
 
+/**
+ * Add parameter.
+ */
 export class Add implements IGun {
+    /**
+     * @param name paramter name
+     * @param adding adding value or lazy-evaluative deal adding value
+     */
     constructor(private readonly name: string,
                 private readonly adding: number | ILazyEvaluative<number>) {}
 
