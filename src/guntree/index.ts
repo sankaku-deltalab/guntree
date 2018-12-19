ca9652f2d89a292d638bd570820a471243c29c88
Sankaku <sankaku_dlt.45631@outlook.jp>
2018-12-19 19:00:23 +0900

    Add index files

---
+++ src/guntree/index.ts
@@ -0,0 +1,16 @@
+import * as gun from './gun';
+import * as lazyEvaluative from './lazyEvaluative';
+import * as parameter from './parameter';
+import * as player from './player';
+
+import * as contents from './contents';
+import * as sweet from './sweet';
+
+export {
+    gun,
+    lazyEvaluative,
+    parameter,
+    player,
+    contents,
+    sweet,
+};
---
+++ src/guntree/sweet/index.ts
@@ -0,0 +1,9 @@
+import * as gun from './gun';
+import * as gunModifier from './gunModifier';
+import * as lazyEvaluative from './lazyEvaluative';
+
+export {
+    gun,
+    gunModifier,
+    lazyEvaluative,
+};
