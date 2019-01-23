1ae7cdb90e096fc2a80672b07dd539913453d60d
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-01-23 18:15:55 +0900

    Add RepeatStateManager used in FiringState

--- src/gun.ts
+++ src/gun.ts
@@ -15,39 +15,14 @@ export interface IFiringState {
     /** Contain data used when fired. */
     fireData: IFireData;
 
+    /** Manager manage repeating while firing. */
+    repeatStates: IRepeatStateManager;
+
     /** Player playing GunTree with this state. */
     player: IPlayer;
 
     /** Copy this state. */
     copy(): IFiringState;
-
-    /**
-     * Get repeat state with name.
-     * If name is not given, return latest repeat state.
-     * If not repeating, return { finished: 0, total: 1 }.
-     *
-     * @param name Repeating name
-     */
-    getRepeatState(name?: string): IRepeatState;
-
-    /**
-     * Notify start repeating.
-     * Called by guns.
-     * Return input repeat state.
-     *
-     * @param state Repeat state
-     * @param name Repeat state name
-     */
-    startRepeating(state: IRepeatState, name?: string): IRepeatState;
-
-    /**
-     * Notify finish repeating.
-     * Started repeating must be finished.
-     *
-     * @param state Repeat state
-     * @param name Repeat state name
-     */
-    finishRepeating(state: IRepeatState, name?: string): void;
 }
 
 /**
@@ -67,82 +42,67 @@ export interface IFireData {
     copy(): IFireData;
 }
 
+/**
+ * IRepeatStateManager manage repeating while firing.
+ */
+export interface IRepeatStateManager {
+    /**
+     * Get repeat state with name.
+     * If name is not given, return latest repeat state.
+     * If not repeating, return { finished: 0, total: 1 }.
+     *
+     * @param name Repeating name
+     */
+    get(name?: string): IRepeatState;
+
+    /**
+     * Notify start repeating.
+     * Called by guns.
+     * Return input repeat state.
+     *
+     * @param state Repeat state
+     * @param name Repeat state name
+     */
+    start(state: IRepeatState, name?: string): IRepeatState;
+
+    /**
+     * Notify finish repeating.
+     * Started repeating must be finished.
+     *
+     * @param state Repeat state
+     * @param name Repeat state name
+     */
+    finish(state: IRepeatState, name?: string): void;
+
+    /** Copy states with manager. */
+    copy(): IRepeatStateManager;
+}
+
 export type TVector2D = {
     x: number,
     y: number,
 };
 
+/**
+ * FiringState contains information while firing.
+ */
 export class FiringState implements IFiringState {
+    /** Contain data used when fired. */
     fireData: IFireData;
-    private readonly repeatStateStack: IRepeatState[];
-    private readonly repeatMap: Map<string, IRepeatState[]>;
+
+    /** Manager manage repeating while firing. */
+    repeatStates: IRepeatStateManager;
 
     constructor(readonly player: IPlayer) {
         this.fireData = new FireData();
-        this.repeatStateStack = [{ finished: 0, total: 1 }];
-        this.repeatMap = new Map();
+        this.repeatStates = new RepeatStateManager();
     }
 
     copy(): FiringState {
         const clone = new FiringState(this.player);
         clone.fireData = this.fireData.copy();
-        for (const rs of this.repeatStateStack) {
-            clone.repeatStateStack.push(rs);
-        }
-        for (const [name, rs] of this.repeatMap) {
-            clone.repeatMap.set(name, rs);
-        }
+        clone.repeatStates = this.repeatStates.copy();
         return clone;
-    }
-
-    private getLatestRepeatState(): IRepeatState {
-        const idx = this.repeatStateStack.length - 1;
-        return this.repeatStateStack[idx];
-    }
-
-    getRepeatState(name?: string): IRepeatState {
-        if (name === undefined) return this.getLatestRepeatState();
-
-        const rsStack = this.repeatMap.get(name);
-        if (rsStack === undefined) throw new Error();
-        return rsStack[rsStack.length - 1];
-    }
-
-    startRepeating(state: IRepeatState, name?: string): IRepeatState {
-        this.repeatStateStack.push(state);
-        if (name !== undefined) {
-            this.setRepeatMap(state, name);
-        }
-        return state;
-    }
-
-    private setRepeatMap(state: IRepeatState, name: string) {
-        if (!this.repeatMap.has(name)) {
-            this.repeatMap.set(name, []);
-        }
-
-        const rsStack = this.repeatMap.get(name);
-        if (rsStack === undefined) throw new Error();
-        rsStack.push(state);
-    }
-
-    finishRepeating(state: IRepeatState, name?: string): void {
-        if (this.repeatStateStack.length === 1) throw new Error('Repeating was finished');
-        if (name !== undefined) {
-            this.popRepeatMap(state, name);
-        }
-        const rs = this.repeatStateStack.pop();
-        if (rs !== state) throw new Error('Finishing repeating is not final repeating');
-    }
-
-    private popRepeatMap(state: IRepeatState, name: string) {
-        const rsStack = this.repeatMap.get(name);
-        if (rsStack === undefined || rsStack.length === 0) {
-            throw new Error(`repeating <${name}> was finished but not repeating`);
-        }
-        const rs = rsStack.pop();
-        if (rs !== state) throw new Error('Finishing repeating is not final repeating');
-        return rs;
     }
 }
 
@@ -172,10 +132,79 @@ export class FireData implements IFireData {
 
 }
 
-const copyMap = <T1, T2>(map: Map<T1, T2>): Map<T1, T2> => {
+export class RepeatStateManager implements IRepeatStateManager {
+    private repeatStateStack: IRepeatState[];
+    private repeatMap: Map<string, IRepeatState[]>;
+
+    constructor() {
+        this.repeatStateStack = [];
+        this.repeatMap = new Map();
+    }
+
+    get(name?: string): IRepeatState {
+        if (name === undefined) {
+            return this.repeatStateStack[this.repeatStateStack.length - 1];
+        }
+
+        const rsStack = this.repeatMap.get(name);
+        if (rsStack === undefined) throw new Error();
+        return rsStack[rsStack.length - 1];
+    }
+
+    start(state: IRepeatState, name?: string): IRepeatState {
+        if (name !== undefined) {
+            this.pushToMap(state, name);
+        }
+
+        this.repeatStateStack.push(state);
+        return state;
+    }
+
+    private pushToMap(state: IRepeatState, name: string): void {
+        const stack = this.repeatMap.get(name);
+        if (stack === undefined) {
+            this.repeatMap.set(name, [state]);
+        } else {
+            stack.push(state);
+        }
+    }
+
+    finish(state: IRepeatState, name?: string): void {
+        if (name !== undefined) {
+            this.popFromMap(state, name);
+        }
+
+        if (this.repeatStateStack.length === 1) {  // First repeating is always [0, 1].
+            throw new Error('Repeating was finished but all repeating was already finished');
+        }
+        const rs = this.repeatStateStack.pop();
+        if (rs !== state) throw new Error('Finished repeating is not current repeating');
+    }
+
+    private popFromMap(state: IRepeatState, name: string) {
+        const stack = this.repeatMap.get(name);
+        if (stack === undefined) throw new Error(`repeating <${name}> is finished but not started repeating`);
+        if (stack.length === 0) throw new Error(`repeating <${name}> is finished but already finished`);
+
+        const rs = stack.pop();
+        if (rs !== state) throw new Error(`Current repeating is not <${name}> but it is finished now`);
+        return rs;
+    }
+
+    /** Copy states with manager. */
+    copy(): RepeatStateManager {
+        const clone = new RepeatStateManager();
+        clone.repeatStateStack = this.repeatStateStack.map(v => Object.assign({}, v));
+        clone.repeatMap = copyMap(this.repeatMap, v => Object.assign({}, v));
+        return clone;
+    }
+}
+
+const copyMap = <T1, T2>(map: Map<T1, T2>, applier?: (value: T2) => T2): Map<T1, T2> => {
     const clone = new Map<T1, T2>();
-    for (const [k, v] of map.entries()) {
-        clone.set(k, v);
+    for (const [k, rawValue] of map.entries()) {
+        const value = applier === undefined ? rawValue : applier(rawValue);
+        clone.set(k, value);
     }
     return clone;
 };
