89c7c66a86d74e373e5295cd8d1ab1f96b36a438
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-01-27 17:51:30 +0900

    Delete ModifierGun

--- src/gun.ts
+++ src/gun.ts
@@ -5,23 +5,6 @@ export interface IGun {
 }
 
 /**
- * ModifierGun modify fireData.
- */
-export class ModifierGun implements IGun {
-    constructor(private readonly modifyTiming: 'immediately' | 'later',
-                private readonly modifierGenerator: (state: IFiringState) => TFireDataModifier) {}
-
-    *play(state: IFiringState): IterableIterator<void> {
-        const modifier = this.modifierGenerator(state);
-        if (this.modifyTiming === 'immediately') {
-            modifier(state, state.fireData);
-        } else {
-            state.pushModifier(modifier);
-        }
-    }
-}
-
-/**
  * Bullet express property of bullet.
  */
 export interface IBullet {}
