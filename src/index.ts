390b13aa7538d13f40587a4ed929e937845d8611
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-03-20 20:39:40 +0900

    Update index.ts to use variables in contens directly

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,4 @@ export * from './lazyEvaluative';
 export * from './parameter';
 export * from './player';
 export * from './transform-util';
-
-import * as elements from './elements';
-import * as contents from './contents';
-
-export { contents, elements };
+export * from './contents';
