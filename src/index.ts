accd41ae39e99f056d8f7b0c37d89fcec19114c1
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-04-04 21:34:29 +0900

    Delete Parameter class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from './gun';
 export * from './firing-state';
 export * from './lazyEvaluative';
-export * from './parameter';
 export * from './player';
 export * from './transform-util';
 export * from './contents';
