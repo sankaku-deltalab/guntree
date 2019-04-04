c9b93bbfb0f71d467036f31731a9f21ded787b32
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-04-04 21:35:12 +0900

    Update index.ts to export muzzle

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './gun';
 export * from './firing-state';
 export * from './lazyEvaluative';
+export * from './muzzle';
 export * from './player';
 export * from './transform-util';
 export * from './contents';
