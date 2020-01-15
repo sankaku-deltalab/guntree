98a911ccccdea4bc8c433f3f8245136b3607d0d2
Sankaku <sankaku_dlt.45631@outlook.jp>
2020-01-15 21:00:48 +0900

    Update transformation-matrix

--- src/transform-util.ts
+++ src/transform-util.ts
@@ -1,4 +1,5 @@
 import * as mat from "transformation-matrix";
+import { Vector } from "./interface";
 
 // https://www.npmjs.com/package/transformation-matrix#data-model
 // const matrix = { a: 1, c: 0, e: 0,
@@ -11,7 +12,7 @@ import * as mat from "transformation-matrix";
  */
 export const decomposeTransform = (
   trans: mat.Matrix
-): [mat.Point, number, mat.Point] => {
+): [Vector, number, Vector] => {
   const loc = { x: trans.e, y: trans.f };
   const scaleX = Math.sqrt(trans.a ** 2 + trans.b ** 2);
   const scaleY = Math.sqrt(trans.c ** 2 + trans.d ** 2);
